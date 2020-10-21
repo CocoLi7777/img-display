3df46d370af068098b204a57c15512d6ab8bb6aa
Redbubble Applicant <me@example.com>
2020-10-21 15:55:15 +1100

    set up the project and make folders

---
+++ src/components/elements/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyledHeader } from './../styles/StyledHeader';
+
+const Header = () => (
+  <StyledHeader>
+    <div>
+      <div className="header-content">Images Pond</div>
+    </div>
+  </StyledHeader>
+);
+
+export default Header;
